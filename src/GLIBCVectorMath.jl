3fc4de899038603893527cffde1a1b1b33571b40
Chris Elrod <elrodc@gmail.com>
2020-08-05 00:24:05 -0400

    Make coefficients global constants.

--- src/GLIBCVectorMath.jl
+++ src/GLIBCVectorMath.jl
@@ -2,10 +2,10 @@ module GLIBCVectorMath
 
 using SIMDPirates
 
-include("exp.jl")
-# include("log.jl")
-
 const F32 = Union{Float32,SVec{<:Any,Float32}}
 const F64 = Union{Float64,SVec{<:Any,Float64}}
+
+include("exp.jl")
+# include("log.jl")
 
 end
--- src/exp.jl
+++ src/exp.jl
@@ -1,29 +1,30 @@
+    # k = 10
+    # Range reduction coefficients:
+    # log(2) inverted = 2^k/ln2 
+const __dbInvLn2 = reinterpret(Float64, 0x40971547652b82fe)
+    # right-shifter value = 3*2^52 
+const __dbShifter = reinterpret(Float64, 0x4338000000000000)
+    # log(2) high part = ln2/2^k(52-k-9 hibits) 
+const __dbLn2hi = reinterpret(Float64, 0x3f462e42fec00000)
+    # log(2) low part = ln2/2^k(52-k-9..104-k-9 lobits) 
+const __dbLn2lo = reinterpret(Float64, 0x3d5d1cf79abc9e3b)
+    # Polynomial coefficients (k=10, deg=3): */
+const __dPC0 = reinterpret(Float64, 0x3ff0000000000000)
+const __dPC1 = reinterpret(Float64, 0x3fe0000001ebfbe0)
+const __dPC2 = reinterpret(Float64, 0x3fc5555555555556)
+    # Other constants:
+    # index mask = 2^k-1 
+const __lIndexMask = 0x00000000000003ff
+    # absolute value mask (SP) 
+# const __iAbsMask = 0x7fffffff
+# const __iDomainRange = 0x4086232a
+
+
 
 const J_TABLE = Float64[2^(big(j-1)/1024) for j in 1:1024];
 # const J_TABLE_ptr = pointer(J_TABLE)
 
 @inline function gexp(x::F64)
-    # k = 10
-    # Range reduction coefficients:
-    # log(2) inverted = 2^k/ln2 
-    __dbInvLn2 = reinterpret(Float64, 0x40971547652b82fe)
-    # right-shifter value = 3*2^52 
-    __dbShifter = reinterpret(Float64, 0x4338000000000000)
-    # log(2) high part = ln2/2^k(52-k-9 hibits) 
-    __dbLn2hi = reinterpret(Float64, 0x3f462e42fec00000)
-    # log(2) low part = ln2/2^k(52-k-9..104-k-9 lobits) 
-    __dbLn2lo = reinterpret(Float64, 0x3d5d1cf79abc9e3b)
-    # Polynomial coefficients (k=10, deg=3): */
-    __dPC0 = reinterpret(Float64, 0x3ff0000000000000)
-    __dPC1 = reinterpret(Float64, 0x3fe0000001ebfbe0)
-    __dPC2 = reinterpret(Float64, 0x3fc5555555555556)
-    # Other constants:
-    # index mask = 2^k-1 
-    __lIndexMask = 0x00000000000003ff
-    # absolute value mask (SP) 
-    # __iAbsMask = 0x7fffffff
-    # __iDomainRange = 0x4086232a
-    
     xint = reinterpret(UInt64, x)
     # xshift = xint >> 32
     dM = fma(x, __dbInvLn2, __dbShifter)
@@ -36,7 +37,8 @@ const J_TABLE = Float64[2^(big(j-1)/1024) for j in 1:1024];
     dR = vfnmadd(dN, __dbLn2lo, dR)
 
     # @show dR
-    expr = fma(fma(fma(__dPC2, dR, __dPC1), dR, __dPC0), dR, __dPC0)
+    # expr = fma(fma(fma(__dPC2, dR, __dPC1), dR, __dPC0), dR, __dPC0)
+    expr = fma(fma(__dPC2, dR, __dPC1), dR, __dPC0) * dR
 
     # iRangeMask = reinterpret(SVec{W,Int32}, iAbsX) > __iDomainRange
 
@@ -45,7 +47,7 @@ const J_TABLE = Float64[2^(big(j-1)/1024) for j in 1:1024];
     lIndex = vload(stridedpointer(J_TABLE), (dMi & __lIndexMask,))
     
     # @show lIndex
-    jR = lIndex * expr
+    jR = fma(expr, lIndex, lIndex)
 
     # @show dN dM jR
     lM = (dMi & (~__lIndexMask)) << 42
@@ -53,35 +55,35 @@ const J_TABLE = Float64[2^(big(j-1)/1024) for j in 1:1024];
     reti = lM + reinterpret(UInt64, jR)
     reinterpret(Float64, reti)
 end
-
-@inline function gexp(x::F32)
     # k = 0
     # log(2) inverted
-    __sInvLn2 = reinterpret(Float32, 0x3fb8aa3b)
+const __sInvLn2 = reinterpret(Float32, 0x3fb8aa3b)
 
     # right shifter constant
-    __sShifter = reinterpret(Float32, 0x4b400000)
+const __sShifter = reinterpret(Float32, 0x4b400000)
 
     # log(2) high part 
-    __sLn2hi = reinterpret(Float32, 0x3f317200)
+const __sLn2hi = reinterpret(Float32, 0x3f317200)
 
     # log(2) low part
-    __sLn2lo = reinterpret(Float32, 0x35bfbe8e)
+const __sLn2lo = reinterpret(Float32, 0x35bfbe8e)
 
     # bias
-    __iBias = 0x0000007f
+const __iBias = 0x0000007f
 
     # Polynomial coefficients:
     # Here we approximate 2^x on [-0.5, 0.5]
-    __sPC0 = reinterpret(Float32, 0x3f800000)
-    __sPC1 = reinterpret(Float32, 0x3f7ffffe)
-    __sPC2 = reinterpret(Float32, 0x3effff34)
-    __sPC3 = reinterpret(Float32, 0x3e2aacac)
-    __sPC4 = reinterpret(Float32, 0x3d2b8392)
-    __sPC5 = reinterpret(Float32, 0x3c07d9fe)
+const __sPC0 = reinterpret(Float32, 0x3f800000)
+const __sPC1 = reinterpret(Float32, 0x3f7ffffe)
+const __sPC2 = reinterpret(Float32, 0x3effff34)
+const __sPC3 = reinterpret(Float32, 0x3e2aacac)
+const __sPC4 = reinterpret(Float32, 0x3d2b8392)
+const __sPC5 = reinterpret(Float32, 0x3c07d9fe)
 
-    # __iAbsMask 0x7fffffff
-    # __iDomainRange 0x42aeac4f
+# const __iAbsMask 0x7fffffff
+# const __iDomainRange 0x42aeac4f
+
+@inline function gexp(x::F32)
     
     sM = fma(__sInvLn2, x, __sShifter)
     sN = sM - __sShifter
